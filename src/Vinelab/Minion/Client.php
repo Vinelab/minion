0230828cbb13843c8f5872305ca13d0f3ac097c8
Abed Halawi <halawi.abed@gmail.com>
2015-02-08 14:07:54 +0200

    move functionality from provider to client class
    The client should have all the functionality related to subscribing,
    registering, publishing and calling so that we get the straight in
    Closure providers.

--- src/Vinelab/Minion/Client.php
+++ src/Vinelab/Minion/Client.php
@@ -9,6 +9,8 @@ use Thruway\Logging\Logger;
  */
 class Client extends \Thruway\Peer\Client {
 
+    protected $topicPrefix = '';
+
     /**
      * The registered providers.
      *
@@ -86,5 +88,136 @@ class Client extends \Thruway\Peer\Client {
     public function getProviders()
     {
         return $this->providers;
+    }
+
+    /**
+     * Call a registered procedure.
+     *
+     * @param string $procedure
+     * @param array  $arguments
+     * @param array  $argumentsKw
+     * @param array  $options
+     *
+     * @return \React\Promise\Promise
+     */
+    public function call($procedure, $arguments = null, $argumentsKw = null, $options = null)
+    {
+        return $this->getSession()->call($this->prepareTopic($procedure), $arguments, $argumentsKw, $options);
+    }
+
+    /**
+     * Subscribe to a topic and specify a callback for it.
+     *
+     * @param string         $topic      The topic name.
+     * @param string|Closure $callback   The callable to be called when the topic received a publish.
+     * @param array          $options    Will be passed straight to thruway @see \Thruway\ClientSession::subscribe
+     * @param boolean        $isFunction Specify whether you're passing in a function from a different scope,
+     *                                   Set this to TRUE to avoid calling the passed callable from this provider's
+     *                                   scope.
+     *
+     * @return \React\Promise\Promise
+     * @see \Thruway\ClientSession::subscribe
+     */
+    public function subscribe($topic, $callback, $options = null, $isFunction = false)
+    {
+        return $this->getSession()->subscribe(
+            $this->prepareTopic($topic),
+            $this->wrapWithProxy($callback, $isFunction),
+            $options
+        );
+    }
+
+    /**
+     * Publish to a topic with the given data.
+     *
+     * @param string      $topic
+     * @param array|mixed $arguments
+     * @param array|mixed $argumentsKw
+     * @param array       $options
+     *
+     * @return \React\Promise\Promise
+     */
+    public function publish($topic, $arguments = null, $argumentsKw = null, $options = null)
+    {
+        return $this->getSession()->publish($this->prepareTopic($topic), $arguments, $argumentsKw, $options);
+    }
+
+    /**
+     * Register a RPC.
+     *
+     * @param string  $topic
+     * @param Closure $callback
+     * @param array   $options
+     * @param bool    $isFunction
+     *
+     * @return \React\Promise\Promise
+     */
+    public function register($topic, $callback, $options = null, $isFunction = false)
+    {
+        return $this->getCallee()->register(
+            $this->getSession(),
+            $this->prepareTopic($topic),
+            $this->wrapWithProxy($callback, $isFunction),
+            $options
+        );
+    }
+
+    /**
+     * Prepare the topic by prefixing it with @property $prefix
+     *  as a convenience for having to manually prefix every topic.
+     *
+     * @param string $topic
+     *
+     * @return string
+     */
+    protected function prepareTopic($topic)
+    {
+        return $this->getTopicPrefix().$topic;
+    }
+
+    /**
+     * Set the topic prefix.
+     *
+     * @param string $prefix
+     *
+     * @return void
+     */
+    public function setTopicPrefix($prefix)
+    {
+        $this->topicPrefix = $prefix;
+    }
+
+    /**
+     * Get the topic prefix.
+     *
+     * @return string
+     */
+    public function getTopicPrefix()
+    {
+        return $this->topicPrefix;
+    }
+
+    /**
+     * Wrap the given callback with a proxy Closure.
+     * The reason we use this is to be able to format the given $data into a Dictionary
+     * which makes it safer to work with them.
+     *
+     * @param  mixed $callback
+     * @param  boolean $isFunction
+     *
+     * @return Closure
+     */
+    public function wrapWithProxy($callback, $isFunction = false)
+    {
+        // We will wrap the callback with a Closure so that we can format the kwArgs that we receive
+        // into our proprietary Dictionary instance to make things safer.
+        return function ($args, $kwArgs) use($callback, $isFunction) {
+
+            if (is_string($callback) && ! $isFunction) {
+                $callback = [$this, $callback];
+            }
+
+            return call_user_func_array($callback, [$args, Dictionary::make($kwArgs)]);
+        };
     }
 }
--- tests/unit/ClientTest.php
+++ tests/unit/ClientTest.php
@@ -1,11 +1,19 @@
 <?php
 
+use Mockery as M;
 use Vinelab\Minion\Client;
 
 /**
  * @author Abed Halawi <abed.halawi@vinelab.com>
  */
-class ClientTest extends PHPUnit_Framework_TestCase {
+class ClientTest extends UnitTestCase {
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->client = M::mock(new Client('i.the.divine', []));
+    }
 
     public function test_initializing_sets_providers()
     {
@@ -34,6 +42,317 @@ class ClientTest extends PHPUnit_Framework_TestCase {
         $this->assertInstanceOf('Vinelab\Minion\Client', $called['bootMe']);
         $this->assertArrayHasKey('andMe', $called);
         $this->assertInstanceOf('Vinelab\Minion\Client', $called['andMe']);
+    }
+
+    public function test_preparing_topic()
+    {
+        $this->client->setTopicPrefix('some.topic.prefix.');
+
+        $prepareTopic = $this->unProtectMethod('prepareTopic', $this->client);
+        $this->assertEquals('some.topic.prefix.my.toppps', $prepareTopic->invokeArgs($this->client, ['my.toppps']));
+
+        $this->assertEquals('some.topic.prefix.', $this->client->getTopicPrefix());
+    }
+
+    public function test_subscribing_with_provider_method()
+    {
+        $session = M::mock('Thruway\ClientSession');
+        $promise = M::mock('React\Promise\Promise');
+        $session->shouldReceive('subscribe')
+            ->once()
+            ->with('my.topic', $this->getProxyCallbackMock(), null)
+            ->andReturn($promise);
+
+        $this->client->shouldReceive('getSession')
+            ->once()
+            ->withNoArgs()
+            ->andReturn($session);
+
+        $subscribe = $this->unProtectMethod('subscribe', $this->client);
+        $got = $subscribe->invokeArgs($this->client, ['my.topic', 'bra']);
+
+        $this->assertEquals($got, $promise);
+    }
+
+    public function test_subscribing_with_options()
+    {
+        $session = M::mock('Thruway\ClientSession');
+        $promise = M::mock('React\Promise\Promise');
+
+        $session->shouldReceive('subscribe')->once()
+            ->with('my.topic', $this->getProxyCallbackMock(), ['option', 'option0'])
+            ->andReturn($promise);
+
+        $this->client->shouldReceive('getSession')->once()->withNoArgs()->andReturn($session);
+
+        $subscribe = $this->unProtectMethod('subscribe', $this->client);
+        $got = $subscribe->invokeArgs($this->client, ['my.topic', 'bra', ['option', 'option0']]);
+
+        $this->assertEquals($promise, $got);
+    }
+
+    public function test_registering_with_options()
+    {
+        $session = M::mock('Thruway\ClientSession');
+        $this->client->shouldReceive('getSession')->once()->withNoArgs()->andReturn($session);
+
+        $callee = M::mock('Thruway\Role\Callee');
+        $promise = M::mock('React\Promise\Promise');
+        $callee->shouldReceive('register')->once()
+            ->with($session, 'reg.topic', $this->getProxyCallbackMock(), ['option', 'option0'])
+            ->andReturn($promise);
+
+        $this->client->shouldReceive('getCallee')->once()->withNoArgs()->andReturn($callee);
+
+        $register = $this->unProtectMethod('register', $this->client);
+        $got = $register->invokeArgs($this->client, ['reg.topic', 'bra', ['option', 'option0']]);
+
+        $this->assertEquals($got, $promise);
+    }
+
+    public function test_registering_with_function()
+    {
+        $session = M::mock('Thruway\ClientSession');
+        $this->client->shouldReceive('getSession')->once()->withNoArgs()->andReturn($session);
+
+        $callee = M::mock('Thruway\Role\Callee');
+        $promise = M::mock('React\Promise\Promise');
+        $callee->shouldReceive('register')->once()->with($session, 'reg.topic', $this->getProxyCallbackMock(), ['option', 'option0'])->andReturn($promise);
+        $this->client->shouldReceive('getCallee')->once()->withNoArgs()->andReturn($callee);
+
+        $register = $this->unProtectMethod('register', $this->client);
+        $got = $register->invokeArgs($this->client, ['reg.topic', 'bra', ['option', 'option0'], true]);
+
+        $this->assertEquals($got, $promise);
+    }
+
+    public function test_registering_prepares_topic()
+    {
+        $this->client->setTopicPrefix('test.test.reg.prefix.');
+
+        $session = M::mock('Thruway\ClientSession');
+        $this->client->shouldReceive('getSession')->once()->withNoArgs()->andReturn($session);
+
+        $callee = M::mock('Thruway\Role\Callee');
+        $promise = M::mock('React\Promise\Promise');
+        $callee->shouldReceive('register')->once()
+            ->with($session, 'test.test.reg.prefix.reg.topic', M::type('Closure'), ['option', 'option0'])
+            ->andReturn($promise);
+
+        $this->client->shouldReceive('getCallee')->once()->withNoArgs()->andReturn($callee);
+
+        $register = $this->unProtectMethod('register', $this->client);
+        $got = $register->invokeArgs($this->client, ['reg.topic', 'bra', ['option', 'option0'], true]);
+
+        $this->assertEquals($got, $promise);
+    }
+
+    public function test_calling_simple()
+    {
+        $session = M::mock('Thruway\ClientSession');
+        $promise = M::mock('React\Promise\Promise');
+        $session->shouldReceive('call')->once()
+            ->with('pub.topic', ['dddddata' => 'hhhhhere'], null, null)
+            ->andReturn($promise);
+
+        $this->client->shouldReceive('getSession')->once()->withNoArgs()->andReturn($session);
+
+        $call = $this->unProtectMethod('call', $this->client);
+        $got = $call->invokeArgs($this->client, ['pub.topic', ['dddddata' => 'hhhhhere']]);
+
+        $this->assertEquals($got, $promise);
+    }
+
+    public function test_calling_full()
+    {
+        $topic       = 'pub.topic';
+        $arguments   = ['dddddata' => 'hhhhhere'];
+        $argumentsKw = ['arg1', 'argw'];
+        $options     = ['some', 'options', 'heeere'];
+
+        $session = M::mock('Thruway\ClientSession');
+        $promise = M::mock('React\Promise\Promise');
+        $session->shouldReceive('call')->once()->with($topic, $arguments, $argumentsKw, $options)->andReturn($promise);
+
+        $this->client->shouldReceive('getSession')->once()->withNoArgs()->andReturn($session);
+
+        $call = $this->unProtectMethod('call', $this->client);
+        $got = $call->invokeArgs($this->client, [$topic, $arguments, $argumentsKw, $options]);
+
+        $this->assertEquals($got, $promise);
+    }
+
+    public function test_calling_prepares_topic()
+    {
+        $topic       = 'pub.topic';
+        $prefixed    = 'test.test.pub.test.pub.topic';
+        $arguments   = ['dddddata' => 'hhhhhere'];
+        $argumentsKw = ['arg1', 'argw'];
+        $options     = ['some', 'options', 'heeere'];
+
+        $this->client->setTopicPrefix('test.test.pub.test.');
+
+        $session = M::mock('Thruway\ClientSession');
+        $promise = M::mock('React\Promise\Promise');
+        $session->shouldReceive('call')->once()->with($prefixed, $arguments, $argumentsKw, $options)->andReturn($promise);
+
+        $this->client->shouldReceive('getSession')->once()->withNoArgs()->andReturn($session);
+
+        $call = $this->unProtectMethod('call', $this->client);
+        $got = $call->invokeArgs($this->client, [$topic, $arguments, $argumentsKw, $options]);
+
+        $this->assertEquals($got, $promise);
+    }
+
+    public function test_registering_with_provider_method()
+    {
+        $session = M::mock('Thruway\ClientSession');
+        $this->client->shouldReceive('getSession')->once()->withNoArgs()->andReturn($session);
+
+        $callee = M::mock('Thruway\Role\Callee');
+        $promise = M::mock('React\Promise\Promise');
+        $callee->shouldReceive('register')->once()->with($session, 'reg.topic', $this->getProxyCallbackMock(), null)->andReturn($promise);
+        $this->client->shouldReceive('getCallee')->once()->withNoArgs()->andReturn($callee);
+
+        $register = $this->unProtectMethod('register', $this->client);
+        $got = $register->invokeArgs($this->client, ['reg.topic', 'bra']);
+
+        $this->assertEquals($got, $promise);
+    }
+
+    public function test_registering_with_closure()
+    {
+        $session = M::mock('Thruway\ClientSession');
+        $this->client->shouldReceive('getSession')->once()->withNoArgs()->andReturn($session);
+
+        $callee = M::mock('Thruway\Role\Callee');
+        $promise = M::mock('React\Promise\Promise');
+        $callee->shouldReceive('register')->once()->with($session, 'reg.topic', $this->getProxyCallbackMock(), null)->andReturn($promise);
+        $this->client->shouldReceive('getCallee')->once()->withNoArgs()->andReturn($callee);
+
+        $callMe = function () { return true; };
+        $register = $this->unProtectMethod('register', $this->client);
+        $got = $register->invokeArgs($this->client, ['reg.topic', $callMe]);
+
+        $this->assertEquals($got, $promise);
+    }
+
+    public function test_publishing_prepares_topic()
+    {
+        $topic       = 'pub.topic';
+        $prefixed    = 'test.test.pub.test.pub.topic';
+        $arguments   = ['dddddata' => 'hhhhhere'];
+        $argumentsKw = ['arg1', 'argw'];
+        $options     = ['some', 'options', 'heeere'];
+
+        $this->client->setTopicPrefix('test.test.pub.test.');
+
+        $session = M::mock('Thruway\ClientSession');
+        $promise = M::mock('React\Promise\Promise');
+        $session->shouldReceive('publish')->once()->with($prefixed, $arguments, $argumentsKw, $options)->andReturn($promise);
+
+        $this->client->shouldReceive('getSession')->once()->withNoArgs()->andReturn($session);
+
+        $publish = $this->unProtectMethod('publish', $this->client);
+        $got = $publish->invokeArgs($this->client, [$topic, $arguments, $argumentsKw, $options]);
+
+        $this->assertEquals($got, $promise);
+    }
+
+    public function test_publishing_simple()
+    {
+        $session = M::mock('Thruway\ClientSession');
+        $promise = M::mock('React\Promise\Promise');
+        $session->shouldReceive('publish')->once()
+            ->with('pub.topic', ['dddddata' => 'hhhhhere'], null, null)
+            ->andReturn($promise);
+
+        $this->client->shouldReceive('getSession')->once()->withNoArgs()->andReturn($session);
+
+        $publish = $this->unProtectMethod('publish', $this->client);
+        $got = $publish->invokeArgs($this->client, ['pub.topic', ['dddddata' => 'hhhhhere']]);
+
+        $this->assertEquals($got, $promise);
+    }
+
+    public function test_subscribing_prepares_topic()
+    {
+        $this->client->setTopicPrefix('test.test.test.prefix.');
+        $session = M::mock('Thruway\ClientSession');
+        $promise = M::mock('React\Promise\Promise');
+        $session->shouldReceive('subscribe')->once()
+            ->with('test.test.test.prefix.my.topic', $this->getProxyCallbackMock(), ['option', 'option0'])
+            ->andReturn($promise);
+
+        $this->client->shouldReceive('getSession')->once()->withNoArgs()->andReturn($session);
+
+        $subscribe = $this->unProtectMethod('subscribe', $this->client);
+        $got = $subscribe->invokeArgs($this->client, ['my.topic', 'whateva', ['option', 'option0'], true]);
+
+        $this->assertEquals($got, $promise);
+    }
+
+    public function test_subscribing_with_function()
+    {
+        $session = M::mock('Thruway\ClientSession');
+        $promise = M::mock('React\Promise\Promise');
+        $session->shouldReceive('subscribe')->once()->with('my.topic', $this->getProxyCallbackMock(), ['option', 'option0'])->andReturn($promise);
+
+        $this->client->shouldReceive('getSession')->once()->withNoArgs()->andReturn($session);
+
+        $subscribe = $this->unProtectMethod('subscribe', $this->client);
+        $got = $subscribe->invokeArgs($this->client, ['my.topic', 'whateva', ['option', 'option0'], true]);
+
+        $this->assertEquals($got, $promise);
+    }
+
+    public function test_subscribing_with_closure()
+    {
+        $session = M::mock('Thruway\ClientSession');
+        $promise = M::mock('React\Promise\Promise');
+        $callMe = function () { return true; };
+        $session->shouldReceive('subscribe')->once()->with('my.topic', $this->getProxyCallbackMock(), ['option', 'option0'])->andReturn($promise);
+
+        $this->client->shouldReceive('getSession')->once()->withNoArgs()->andReturn($session);
+
+        $subscribe = $this->unProtectMethod('subscribe', $this->client);
+        $got = $subscribe->invokeArgs($this->client, ['my.topic', $callMe, ['option', 'option0']]);
+
+        $this->assertEquals($got, $promise);
+    }
+
+    public function test_publishing_full()
+    {
+        $topic       = 'pub.topic';
+        $arguments   = ['dddddata' => 'hhhhhere'];
+        $argumentsKw = ['arg1', 'argw'];
+        $options     = ['some', 'options', 'heeere'];
+
+        $session = M::mock('Thruway\ClientSession');
+        $promise = M::mock('React\Promise\Promise');
+        $session->shouldReceive('publish')->once()->with($topic, $arguments, $argumentsKw, $options)->andReturn($promise);
+
+        $this->client->shouldReceive('getSession')->once()->withNoArgs()->andReturn($session);
+
+        $publish = $this->unProtectMethod('publish', $this->client);
+        $got = $publish->invokeArgs($this->client, [$topic, $arguments, $argumentsKw, $options]);
+
+        $this->assertEquals($got, $promise);
+    }
+
+    protected function getProxyCallbackMock()
+    {
+        return M::on(function ($proxy) {
+            $this->assertTrue(is_callable($proxy));
+
+            $reflection = new ReflectionFunction($proxy);
+            $params  = $reflection->getParameters();
+
+            $this->assertEquals('args', $params[0]->name);
+            $this->assertEquals('kwArgs', $params[1]->name);
+
+            return true;
+        });
     }
 }
 
